4d12c0c9000d1820ed293eac130842b6ecb07252
Jay Choy <zcabzjc@ucl.ac.uk>
2023-10-08 13:05:05 +0100

    added client and server

--- server/main.go
+++ server/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"log"
+	"net"
+	pb "github.com/ZJay07/Go-gRPC-example/proto"
+
+	"google.golang.org/grpc"
+
+)
+
+const (
+	port = ":8080"
+)
+
+type helloServer struct{
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to start the server %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("server started at %v", list.Addr())
+    if err := grpcServer.Serve(lis); err != nil{
+		log.Fatalf("Failed to start: %v", err)
+	}
+}
---
+++ server/unary.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"context"
+	pb "github.com/ZJay07/Go-gRPC-example"
+)
+
+func(s *helloServer) SayHello(ctx context.Context, req *pb.NoParam) (*pb.HelloResponse, error){
+	return &pb.HelloResponse{
+		Message: "Hello",
+	}, nil
+}
